e317592f84268460ad7be76724d55931328d83d3
cooperdebear <cooperdebear@web.de>
2017-02-12 16:52:55 +0100

    Changed to Maven

--- ListProject/src/main/java/ListModul/List.java
+++ ListProject/src/main/java/ListModul/List.java
@@ -1,4 +1,4 @@
-package ListenModul;
+package ListModul;
 
 /**
  * @author cooperdebear
--- ListProject/src/main/java/ListModul/ListHandler.java
+++ ListProject/src/main/java/ListModul/ListHandler.java
@@ -1,4 +1,4 @@
-package ListenModul;
+package ListModul;
 
 import java.util.Scanner;
 import java.util.InputMismatchException;
